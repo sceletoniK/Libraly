1b2a49ea7590c386d44ecaa13f5f778fa7fffeb3
sceletoniK <L.s1n56@yandex.ru>
2023-02-25 16:53:41 +0300

    изменил бд для продолжительности аренды

--- DB/DB.go
+++ DB/DB.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
