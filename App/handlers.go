3b2f490e178404bf709fc1bfdcf9d48bfbe2a1bd
sceletoniK <L.s1n56@yandex.ru>
2023-02-25 19:02:42 +0300

    Добавил handler на добавление книг

--- App/handlers.go
+++ App/handlers.go
@@ -1,1 +1,24 @@
 package app
+
+import (
+	"net/http"
+
+	models "github.com/sceletoniK/Models"
+)
+
+func (s *Server) handlerNewBook(w http.ResponseWriter, r *http.Request) {
+	s.logger.Info("Try to insert new Book")
+	var book models.NewBook
+	err := s.bodyParse(r, &book)
+	if err != nil {
+		s.httpError(w, r, 500, err)
+		s.logger.Error(err)
+		return
+	}
+	if err = s.db.AddBook(book); err != nil {
+		s.httpError(w, r, 500, err)
+		s.logger.Error(err)
+		return
+	}
+	s.logger.Info("Success")
+}
