5250aa6872cad449c2e094321c7f7320740c3ccc
sceletoniK <L.s1n56@yandex.ru>
2023-03-07 09:37:38 +0300

    добавил логгер в middleware проверки access токена

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,20 +1,20 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/sirupsen/logrus"
 )
 
-func Auth(key []byte) func(next http.Handler) http.Handler {
+func Auth(key []byte, lg *logrus.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			header := r.Header.Get("Authorization")
+			header := r.Header.Get("Middleware: Authorization")
 			bearer := strings.Split(header, " ")
 			if len(bearer) != 2 {
-				log.Println("Middleware: 401")
+				lg.Info("Middleware: 401")
 				w.WriteHeader(401)
 				return
 			}
@@ -24,19 +24,19 @@ func Auth(key []byte) func(next http.Handler) http.Handler {
 				return key, nil
 			})
 			if err != nil {
-				log.Println(err)
+				lg.Info("Middleware: ", err)
 				w.WriteHeader(401)
 				return
 			}
 			if !b.Valid {
-				log.Default().Println("not valid token")
+				lg.Info("Middleware: not valid token")
 				w.WriteHeader(401)
 				return
 			}
 
 			_, ok := b.Claims.(*AuthClaims)
 			if !ok {
-				log.Default().Println("claims", ok)
+				lg.Info("Middleware: claims - ", ok)
 				w.WriteHeader(500)
 				return
 			}
