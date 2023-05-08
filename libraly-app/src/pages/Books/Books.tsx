b6201ec88d222f3e7da3a0826d3b3cd37a20e329
sceletoniK <L.s1n56@yandex.ru>
2023-05-08 21:51:17 +0300

    changed book table to list

--- libraly-app/src/pages/Books/Books.tsx
+++ libraly-app/src/pages/Books/Books.tsx
@@ -1,15 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react'
-import type { ColumnsType, ColumnType } from 'antd/es/table';
-import type { InputRef } from 'antd';
-import type { FilterConfirmProps } from 'antd/es/table/interface';
-import { Button, Input, Space, Table, Col, Row } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import React, { useState, useEffect} from 'react'
+import {List, Button, Input, Space, Col, Row, Typography, Divider, Form, Card, theme, Tag} from 'antd';
+import {Link} from "react-router-dom";
+import axios from "axios";
+import {resolveObjectURL} from "buffer";
+const { Title, Text } = Typography;
+const { CheckableTag } = Tag;
 
 const Books: React.FC = () => {
 
   const [error, setError] = useState<Error|null>(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState<DataType[]>([]);
+
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [genres, setGenres] = useState<GenreType[]>([]);
+
+
+
 
   useEffect(() => {
     fetch("http://localhost:8080/book")
@@ -23,113 +30,35 @@ const Books: React.FC = () => {
           setIsLoaded(true);
           setError(error);
         }
-      )
+      );
+    axios.get(
+          "http://localhost:8080/genre"
+      ).then((result) => {
+          console.log('Получилось')
+          setGenres(result.data)
+      }).catch((error) => {
+          console.log('Пятисотая на проде!')
+      })
   }, [])
 
   interface DataType {
     id: React.Key,
     name : string,
     author : string,
-    publisher : string
+    publisher : string,
   }
 
-  type DataIndex = keyof DataType;
+  interface GenreType {
+      genreId: React.Key,
+      name : string,
+  }
 
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
-  const searchInput = useRef<InputRef>(null);
-
-  const handleSearch = (
-    selectedKeys: string[],
-    confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: DataIndex,
-  ) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = (clearFilters: () => void) => {
-    clearFilters();
-    setSearchText('');
-  };
-
-  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    }
-  });
-
-  const columns : ColumnsType<DataType> = [
-    {
-      title: 'Название',
-      dataIndex: 'name',
-      key: 'name',
-      ...getColumnSearchProps('name'),
-    },
-    
-    {
-      title: 'Автор',
-      dataIndex: 'author',
-      key: 'author',
-      ...getColumnSearchProps('author'),
-    },
-    {
-      title: 'Издатель',
-      dataIndex: 'publisher',
-      key: 'publisher',
-      ...getColumnSearchProps('publisher'),
-    },
-  ];
+    const handleChange = (tag: string, checked: boolean) => {
+        const nextSelectedTags = checked
+            ? [...selectedTags, tag]
+            : selectedTags.filter((t) => t !== tag);
+        setSelectedTags(nextSelectedTags);
+    };
 
   if (error) {
     return <div>Ошибка: {error.message}</div>;
@@ -137,13 +66,105 @@ const Books: React.FC = () => {
     return <div>Загрузка...</div>;
   } else {
   return (
-    <>
-    <Row justify='center'>
-      <Col span={18}>
-        <Table bordered dataSource={items} columns={columns} />
+    <Row justify='center' gutter={40}>
+      <Col span={4}>
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 18 }}
+            initialValues={{ remember: true }}
+            autoComplete="off"
+        >
+            <Title level={3}>Фильтр</Title>
+          <Form.Item
+              label="Название"
+              name="name"
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+              label="Автор"
+              name="author"
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+              label="Издатель"
+              name="pubisher"
+          >
+            <Input />
+          </Form.Item>
+            <Space size={[0, 8]} wrap>
+                {genres.map((tag) => (
+                    <CheckableTag
+                        key={tag.genreId}
+                        checked={selectedTags.includes(tag.name)}
+                        onChange={(checked) => handleChange(tag.name, checked)}
+                    >
+                        {tag.name}
+                    </CheckableTag>
+                ))}
+            </Space>
+            <br/>
+            <br/>
+          <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
+            <Space size='large'>
+            <Button type="primary" htmlType="submit">
+              Поиск
+            </Button>
+            <Button htmlType="button">
+              Очистить
+            </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Col>
+      <Col span={20}>
+        <List
+            itemLayout="vertical"
+            size="large"
+            pagination={{
+              pageSize: 12,
+              position: 'bottom',
+              align: 'center'
+            }}
+            grid={{
+              gutter: 20,
+              xs: 1,
+              sm: 2,
+              md: 2,
+              lg: 3,
+              xl: 3,
+              xxl: 3,
+            }}
+            dataSource={items}
+            renderItem={(item: DataType) => (
+                <List.Item
+                    key={item.id}
+                >
+                  <Link to={`/book/${item.id}`}>
+                  <Card
+                      hoverable
+                      style={{ width: 300 }}
+                      cover={<img
+                          width={272}
+                          alt="logo"
+                          src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                      />}
+                  >
+                    <Title level={2}>{item.name}</Title>
+                    <Space direction="vertical">
+                      <Text >{item.author}</Text>
+                      <Text type="secondary">{item.publisher}</Text>
+                    </Space>
+                  </Card>
+                  </Link>
+                </List.Item>
+            )}/>
       </Col>
     </Row>
-    </>
   );
   }
 }
